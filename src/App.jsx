5b6ab24fa959a155c54e49f02866a6c1c4659d26
jen <jen@reiher.ca>
2016-10-29 20:32:22 -0700

    Data is grouped by date and displays on page

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,59 @@ import moment from 'moment';
 
 class App extends React.Component {
   renderTodos() {
-    let data = [
-      {
-        id: 0,
-        timestamp: '1477789154',
-        status: 1,
-        contents: 'My first todo'
-      },
-      {
-        id: 1,
-        timestamp: '1477789247',
-        status: 1,
-        contents: 'Display on page'
-      },
-      {
-        id: 2,
-        timestamp: '1477789224',
-        status: 1,
-        contents: 'Toggle status'
-      },
-      {
-        id: 3,
-        timestamp: '1477789512',
-        status: 0,
-        contents: 'Group todos by date'
-      },
-      {
-        id: 4,
-        timestamp: '1477789272',
-        status: 0,
-        contents: 'If status changes to 1 copy to next days date'
-      }
+    let data =
+      [ 
+        { 
+          '2016-10-30': [
+            {
+              id: 0,
+              timestamp: '2016-10-30',
+              status: 1,
+              contents: 'My first todo'
+            },
+            {
+              id: 1,
+              timestamp: '2016-10-30',
+              status: 1,
+              contents: 'Display on page'
+            },
+            {
+              id: 2,
+              timestamp: '2016-10-30',
+              status: 1,
+              contents: 'Toggle status'
+            }
+          ]
+        },
+        {
+          '2016-10-31': [
+            {
+              id: 3,
+              timestamp: '2016-10-31',
+              status: 0,
+              contents: 'Group todos by date'
+            },
+            {
+              id: 4,
+              timestamp: '2016-10-31',
+              status: 0,
+              contents: 'If status changes to 1 copy to next days date'
+            } 
+          ]
+        }
     ];
 
     return(
       <div>
-        {data.map(todo=> (
-          <Todo data={todo} key={todo.id} />
+        {data.map((date, index)=> (
+          <div>
+            <h2 key={index}>{moment(Object.keys(date)[0]).format('MMMM Do[,] YYYY')}</h2>
+            <div>
+              {date[Object.keys(date)].map(todo=> (
+                <Todo data={todo} key={todo.id} />
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 class Todo extends React.Component {
   constructor(props) {
