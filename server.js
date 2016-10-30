333ebd682939fa74351117fedd1ed1061a1ba6fd
jen <jen@reiher.ca>
2016-10-29 18:50:30 -0700

    Todos render to the page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+"use strict";
+
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
@@ -16,3 +18,50 @@ new WebpackDevServer(webpack(config), {
 
     console.log('Running at http://0.0.0.0:3000');
   });
+
+// require('dotenv').config();
+
+// const PORT        = process.env.PORT || 8080;
+// const ENV         = process.env.ENV || "development";
+// const express     = require("express");
+// const bodyParser  = require("body-parser");
+// const sass        = require("node-sass-middleware");
+// const app         = express();
+
+// const knexConfig  = require("./knexfile");
+// const knex        = require("knex")(knexConfig[ENV]);
+// const morgan      = require('morgan');
+// const knexLogger  = require('knex-logger');
+
+// // Seperated Routes for each Resource
+// const usersRoutes = require("./routes/users");
+
+// // Load the logger first so all (static) HTTP requests are logged to STDOUT
+// // 'dev' = Concise output colored by response status for development use.
+// //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// app.use(morgan('dev'));
+
+// // Log knex SQL queries to STDOUT as well
+// app.use(knexLogger(knex));
+
+// app.set("view engine", "ejs");
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use("/styles", sass({
+//   src: __dirname + "/styles",
+//   dest: __dirname + "/public/styles",
+//   debug: true,
+//   outputStyle: 'expanded'
+// }));
+// app.use(express.static("public"));
+
+// // Mount all resource routes
+// app.use("/api/users", usersRoutes(knex));
+
+// // Home page
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// app.listen(PORT, () => {
+//   console.log("Example app listening on port " + PORT);
+// });
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Todo from './components/Todo.jsx'
 
-class App extends Component {
+class App extends React.Component {
+  renderTodos() {
+    let data = [
+      {
+        id: 0,
+        timestamp: '1477789154',
+        status: 1,
+        contents: 'My first todo'
+      },
+      {
+        id: 1,
+        timestamp: '1477789247',
+        status: 1,
+        contents: 'Display on page'
+      },
+      {
+        id: 2,
+        timestamp: '1477789224',
+        status: 0,
+        contents: 'Toggle status'
+      },
+      {
+        id: 3,
+        timestamp: '1477789272',
+        status: 0,
+        contents: 'If status changes to 1 copy to next days date'
+      },
+      {
+        id: 4,
+        timestamp: '1477789512',
+        status: 0,
+        contents: 'Group todos by date'
+      }
+    ];
+
+    return(
+      <div>
+        {data.map(todo=> (
+          <Todo status={todo.status} contents={todo.contents} key={todo.id} />
+        ))}
+      </div>
+    );
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Hello React :)</h1>
+        {this.renderTodos()}
+      </div>
     );
   }
 }
