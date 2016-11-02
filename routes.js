25c6698d78727f07a8c25ad273a5e3bfc1525381
jen <jen@reiher.ca>
2016-11-02 09:44:50 -0700

    Remove unnecessary jquery

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = (knex) => {
+  
   // define the home page route
   router.get('/todos', function(req, res) {
     knex
@@ -14,14 +15,11 @@ module.exports = (knex) => {
   });
 
   router.get('/todos/new', function(req, res) {
-    // remove this for production
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
+    res.setHeader('Access-Control-Allow-Origin', '*'); // remove this for production
   })
   // define the about route
   router.post('/todos/new', function(req, res) {
-    // remove this for production
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // remove this for production
     try{req.body = JSON.parse(Object.keys(req.body)[0])}catch(err){req.body = req.body}
     res.json(req.body)
   });
